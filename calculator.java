54835a3b0f6c3c5e7d31221bb6239abc1f7a97a9
Chung Nguyen <chung.nguyen@gmail.com>
2017-01-30 15:50:57 -0800

    Finished Calculator practice (Java)

---
+++ calculator.java
@@ -0,0 +1,55 @@
+public class Calculator {
+
+  // Create a calculator class
+
+  public Calculator() {
+
+  }
+
+  public int add(int a, int b) {
+
+    return a + b;
+
+  }
+
+  public int subtract(int a, int b) {
+
+    return a - b;
+
+  }
+
+  public int multiply(int a, int b) {
+
+    return a * b;
+
+  }
+
+  public int divide(int a, int b) {
+
+    if (b == 0) {
+      System.out.println("Error! Dividing by zero is not allowed.");
+      return 0;
+    } else {
+      return a / b;
+    }
+  }
+
+  public int modulo(int a, int b) {
+    if (b == 0) {
+      System.out.println("Error! Dividing by zero is not allowed.");
+      return 0;
+    } else {
+      return a % b;
+    }
+
+    }
+
+  public static void main(String[] args) {
+
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+    System.out.println(myCalculator.subtract(45, 11));
+
+  }
+
+}
